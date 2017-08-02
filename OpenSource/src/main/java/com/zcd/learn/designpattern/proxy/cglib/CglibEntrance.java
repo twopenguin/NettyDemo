1036f63594b3b446ca936dc4dd8e6df3b00f667a
twopenguin <1289410799@qq.com>
2017-08-02 22:04:31 +0800

    “策略模式代码结构

--- OpenSource/src/main/java/com/zcd/learn/designpattern/proxy/cglib/CglibEntrance.java
+++ OpenSource/src/main/java/com/zcd/learn/designpattern/proxy/cglib/CglibEntrance.java
@@ -3,7 +3,7 @@ package com.zcd.learn.designpattern.proxy.cglib;
 /**
  * Created by admin on 2017/8/2.
  */
-public class Entrance {
+public class CglibEntrance {
     public static void main(String[] args) {
         CglibProxy proxy = new CglibProxy();
         // base为生成的增强过的目标类
---
+++ OpenSource/src/main/java/com/zcd/learn/designpattern/strategy/StrategyEntrance.java
@@ -0,0 +1,10 @@
+package com.zcd.learn.designpattern.strategy;
+
+/**
+ * Created by zhangyida on 2017/8/2.
+ */
+public class StrategyEntrance {
+    public static void main(String[] args) {
+
+    }
+}
