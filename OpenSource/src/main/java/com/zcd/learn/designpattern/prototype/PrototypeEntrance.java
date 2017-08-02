8703bde3714d2647005b754f264b0d91959f2c72
twopenguin <1289410799@qq.com>
2017-08-02 22:22:13 +0800

    原型模式结构提交！

---
+++ OpenSource/src/main/java/com/zcd/learn/designpattern/prototype/PrototypeEntrance.java
@@ -0,0 +1,16 @@
+package com.zcd.learn.designpattern.prototype;
+
+/**
+ *
+ */
+public class PrototypeEntrance {
+    /**
+     * 有一个现成的对象，这个对象里面有设置好的值，
+     * 当我要新建一个对象，并且要给新建的对象赋值，而且赋值内容跟以前的一模一样
+     * orm 里面经常使用
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
