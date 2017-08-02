62dd46bd5ce30faad3cac7b66badf86c2466ba6b
twopenguin <1289410799@qq.com>
2017-08-02 23:43:44 +0800

    原型模式代码提交！

--- OpenSource/src/main/java/com/zcd/learn/designpattern/prototype/PrototypeEntrance.java
+++ OpenSource/src/main/java/com/zcd/learn/designpattern/prototype/PrototypeEntrance.java
@@ -8,6 +8,7 @@ public class PrototypeEntrance {
      * 有一个现成的对象，这个对象里面有设置好的值，
      * 当我要新建一个对象，并且要给新建的对象赋值，而且赋值内容跟以前的一模一样
      * orm 里面经常使用
+     * clone 不会调用构造方法，而是直接复制内存中的字节
      * @param args
      */
     public static void main(String[] args) {
